92eee0a4d2cd03101b32e00f59696235964ba38e
ISHIKAWA Takayuki <topstone@users.noreply.github.com>
2026-07-08 12:21:10 +0900

    1fst commit

---
+++ lib/moodle_assigntext_converter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "moodle_assigntext_converter/version"
+
+module MoodleAssigntextConverter
+  class Error < StandardError; end
+  # Your code goes here...
+end
